e2a4fc41197fead70425c1cd2bf0a6df04ad5b4c
Alida Gorman <128799448+atgorman1@users.noreply.github.com>
2023-05-24 10:57:35 -0400

    Added functions to validate the guess is a letter that has not already been guessed and add to array if so

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Unordered list where guessed letters will appear
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersUl = document.querySelector(".guessed-letters");
 // Button with “Guess!” text
 const guessButton = document.querySelector(".guess");
 // Text input where player enters a letter to guess
@@ -11,15 +11,18 @@ const remainingGuesses = document.querySelector(".remaining");
 // <span> inside <p> that shows how many guesses are left
 const numGuessesLeft = document.querySelector(".remaining span");
 // Empty <p> where messages to players appear
-const messagesToUser = document.querySelector(".message");
+const msgToUser = document.querySelector(".message");
 // Button that prompts users to play again (hidden at first)
 const playAgainButton = document.querySelector(".play-again");
 
 // Starter word
 const word = "magnolia";
 
+// letters that the player has guessed
+const guessedLetters = [];
+
+// function that replaces the letters of the word with circles before letters are guessed
 const hideLetters = function (word) {
-  // function with the starter word getting passed to it
   const placeholderLetters = []; // new blank array that circles will get added to
   for (const letter of word) {
     // for every letter in the word
@@ -28,12 +31,45 @@ const hideLetters = function (word) {
   }
   inProgWord.innerText = placeholderLetters.join(""); // combine all of the objects in the array (all the circles) into one string and add it to the word in progress <p>
 };
-hideLetters(word);
+hideLetters(word); // call function so letters will be replaced with circles
 
-// Event Listener for Guess Button - When button is clicked, store letter value in conole unthe letterInput variable, and then change the input box back to blank.
+// Event Listener for Guess Button - When button is clicked, store letter value in console  of the guess variable, and then change the input box back to blank.
 guessButton.addEventListener("click", function (e) {
   e.preventDefault();
-  const letterInput = inputLetterBox.value;
-  console.log(letterInput);
+  msgToUser.innerText = "";
+  const guess = inputLetterBox.value;
+  // console.log(guess);
+  const validGuess = inputValidation(guess);
+  if (validGuess) {
+    makeGuess(validGuess);
+  }
   inputLetterBox.value = "";
 });
+
+// function expression that validates the player's input
+const inputValidation = function (input) {
+  const acceptedLetter = /[a-zA-Z]/;
+  // if input is empty
+  if (input.length === 0) {
+    msgToUser.innerText = "Enter a letter to make a guess.";
+    // if input is more than one letter
+  } else if (input.length > 1) {
+    msgToUser.innerText = "One letter at a time!";
+    // if input is not a letter
+  } else if (!input.match(acceptedLetter)) {
+    msgToUser.innerText = `${input} is not a letter. Please enter a letter to make a guess. :)`;
+  } else {
+    return input;
+  }
+};
+
+// function that checks if a letter was already guessed, let the player know.  If not, add to the guessed letters array.
+const makeGuess = function (guess) {
+  guess = guess.toUpperCase();
+  if (guessedLetters.includes(guess)) {
+    msgToUser.innerText = `You already guessed ${guess}. Try guessing another letter! :)`;
+  } else {
+    guessedLetters.push(guess);
+    console.log(guessedLetters);
+  }
+};
